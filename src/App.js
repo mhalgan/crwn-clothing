af9a9a8262c897ae09a16d6e4d6e677035b5714c
mhalgan <gccr92@gmail.com>
2020-01-20 23:34:51 -0300

    Added SASS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./homepage.component";
+
 function App() {
-  return <div>Hello World!</div>;
+  return <HomePage />;
 }
 
 export default App;
