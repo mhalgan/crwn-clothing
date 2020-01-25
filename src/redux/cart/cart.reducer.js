6df1e32f36efe9f70dfc6bfe9b9672d3dbf79dd1
mhalgan <gccr92@gmail.com>
2020-01-25 15:29:46 -0300

    added cart-item and displayed it on cart-dropdown

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -3,7 +3,7 @@ import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
-  cartItens: []
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -16,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItens: addItemToCart(state.cartItens, action.payload)
+        cartItems: addItemToCart(state.cartItems, action.payload)
       };
     default:
       return state;
