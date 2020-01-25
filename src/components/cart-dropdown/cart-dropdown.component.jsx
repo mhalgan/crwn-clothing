4e18b54f9a78e367f1636b05e90c6b8a898dbd52
mhalgan <gccr92@gmail.com>
2020-01-25 18:27:08 -0300

    improved user flow by hidding cart-dropdown when redirecting to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,12 +4,14 @@ import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router-dom";
 
 import CustomButton from "../custom-button/custom-button.component";
-import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/cart-item.component";
+
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toogleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -20,7 +22,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push("/checkout")}>
+    <CustomButton
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toogleCartHidden());
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
