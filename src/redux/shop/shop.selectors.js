da94b51feba34fb5f21dde6f5164dd8b564ec611
Glauber Camargo Campos Rocha <gccr92@gmail.com>
2020-01-30 16:32:45 -0300

    implemented async fetch in redux-thunk

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -18,3 +18,8 @@ export const selectCollection = collectionUrlParam =>
   createSelector([selectCollections], collections =>
     collections ? collections[collectionUrlParam] : null
   );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
