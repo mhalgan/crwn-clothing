d38d4b4c8708b5b0bb7a7f357ee5584deafe58c5
Glauber Camargo Campos Rocha <gccr92@gmail.com>
2020-02-03 13:46:59 -0300

    added backend to handle Stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   // Price in cents
@@ -8,7 +9,24 @@ const StripeCheckoutButton = ({ price }) => {
 
   const onToken = token => {
     console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      baseURL: "http://localhost:5000",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("Payment successful");
+      })
+      .catch(error => {
+        console.log("Payment error: ", error);
+        alert(
+          "There was an issue with your payment. Please use the provided credentials."
+        );
+      });
   };
 
   return (
